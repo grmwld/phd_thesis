dc86aa82ea1b0b068c29779884f368ce20850ec9
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2014-04-08 20:34:25 +0200

    Delete temp files

--- make-pdf.sh
+++ make-pdf.sh
@@ -2,6 +2,8 @@ PDFLATEX="/usr/texbin/pdflatex -synctex=2 -interaction=nonstopmode main.tex"
 BIBTEX="/usr/texbin/bibtex main.aux"
 GLOS="makeglossaries main"
 
+rm main.acn main.acr main.alg main.idx main.ist main.glsdefs main.aux main.bbl main.blg main.lof main.lot main.toc
+
 $PDFLATEX
 $BIBTEX
 $GLOS
